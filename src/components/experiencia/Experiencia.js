4fbf415592de7d8b0493a5add57ed8c72385ff35
Camilo Franco Moya <69817111+Aleksei324@users.noreply.github.com>
2023-03-13 17:45:18 -0500

    updating about page

--- src/components/experiencia/Experiencia.js
+++ src/components/experiencia/Experiencia.js
@@ -8,66 +8,61 @@ import imagesql from '../../images/sql.png';
 import imagejava from '../../images/java.png';
 
 function Experiencia() {
-    return (
-        <div>
-            <br/>
-            <br/>
-            <br/>
-            <h1 className='titleSubpage'>Some projects</h1>
-            <section className='Experiencia flex'>
-                
-                <div className='border flex'>
-                    <div className='mar'>
-                        <h1>Numfinity</h1>
-                        <p><i>Work in progress project:</i> A module
-                        for <i>Python 3</i> with which you can create dynamically
-                        sized numbers; in other words, increase their maximum
-                        decimal or digit capacity to whatever is required.</p>
-                        <img className='iconGrow' src={imagepy} alt='Python icon' title='Python 3' width='32' height='32'/>
-                    </div>
-                    <ExperienciaButton link="https://github.com/Aleksei324/Numfinity"/>
-                </div>
+	return (
+		<div>
+			<br/><br/><br/>
 
-                <div className='border flex'>
-                    <div className='mar'>
-                        <h1>Betterzon</h1>
-                        <p><i>Innovation prototype as a university
-                        project:</i> An <i>Android</i> application
-                        to communicate via chat to construction
-                        professionals with people who need their
-                        services, in addition to having a section
-                        where you can check prices of construction
-                        materials. It was built with <i>Java</i> and
-                        a <i>MySQL</i> database.</p>
-                        <img className='iconGrow' src={imagejava} alt='Java icon' title='Java 8' width='32' height='32'/>
-                        <img className='iconGrow' src={imagesql} alt='SQL icon' title='MySQL' width='32' height='32'/>
-                    </div>
-                    <ExperienciaButton link="https://github.com/Aleksei324/Betterzon"/>
-                </div>
+			<h1 className='titleSubpage'>Some projects</h1>
+			<section className='Experiencia flex'>
 
-                <div className="border flex">
-                    <div className='mar'>
-                        <h1>This webpage!</h1>
-                        <p>Practice website that I built by myself
-                        with my personal information, data about
-                        some of my own projects and a mini-game.
-                        It is built in <i>React</i>.</p>
-                        <img className='iconGrow' src={imagereact} alt='React icon' title='React' width='32' height='32'/>
-                        <img className='iconGrow' src={imagecss} alt='CSS icon' title='CSS 3' width='32' height='32'/>
-                    </div>
-                    <ExperienciaButton link="https://github.com/Aleksei324/webpage"/>
-                </div>
+				<div className='border flex'>
+					<div className='mar'>
+						<h1>Numfinity</h1>
+						<p><i>Work in progress project:</i> A module
+						for <i>Python 3</i> with which you can create dynamically
+						sized numbers; in other words, increase their maximum
+						decimal or digit capacity to whatever is required.</p>
+						<img className='iconGrow' src={imagepy} alt='Python icon' title='Python 3' width='40' height='40'/>
+					</div>
+					<ExperienciaButton link="https://github.com/Aleksei324/Numfinity"/>
+				</div>
 
-            </section>
+				<div className='border flex'>
+					<div className='mar'>
+						<h1>Betterzon</h1>
+						<p><i>Innovation prototype as a university
+						project:</i> An <i>Android</i> application
+						to communicate via chat to construction
+						professionals with people who need their
+						services, in addition to having a section
+						where you can check prices of construction
+						materials. It was built with <i>Java</i> and
+						a <i>MySQL</i> database.</p>
+						<img className='iconGrow' src={imagejava} alt='Java icon' title='Java 8' width='40' height='40'/>
+						<img className='iconGrow' src={imagesql} alt='SQL icon' title='MySQL' width='40' height='40'/>
+					</div>
+					<ExperienciaButton link="https://github.com/Aleksei324/Betterzon"/>
+				</div>
 
-            <br/>
-            <p>... and some more in <a target='_blank' rel="noreferrer" href='https://github.com/Aleksei324' title='Link to my personal Github.'>my Github.</a></p>
-            <br/>
-            
-            <br/><br/><br/><br/><br/>
-            <br/><br/>
-        </div>
-    );
+				<div className="border flex">
+					<div className='mar'>
+						<h1>This webpage!</h1>
+						<p>Practice website that I built by myself
+						with my personal information, data about
+						some of my own projects and a mini-game.
+						It is built in <i>React</i>.</p>
+						<img className='iconGrow' src={imagereact} alt='React icon' title='React' width='40' height='40'/>
+						<img className='iconGrow' src={imagecss} alt='CSS icon' title='CSS 3' width='40' height='40'/>
+					</div>
+					<ExperienciaButton link="https://github.com/Aleksei324/webpage"/>
+				</div>
+
+			</section>
+			<br/>
+			<p>... and some more in <a target='_blank' rel="noreferrer" href='https://github.com/Aleksei324' title='Link to my personal Github.'>my Github.</a></p>
+			<br/>
+		</div>
+	);
 }
 
 export default Experiencia;
--- src/components/main/AboutMe.js
+++ src/components/main/AboutMe.js
@@ -2,56 +2,65 @@ import './AboutMe.css';
 import imagecode from '../../images/code.jpg';
 
 function AboutMe(){
-    return (
-        <section className="flex AboutMe">
-            <div className='centerSideMain mar'>
-                
-                <img className='imgCool imgMain' src={imagecode} alt='A computer with code and a cup' width='300' height='200'/>
-                <h2>About me</h2>
+	return (
+		<section className="flex AboutMe">
+			<div className='centerSideMain mar'>
 
-                <p>Welcome to my webpage, I'm just a guy who likes to develop software and webpages, play games and play bass a little;
-                I like to tinker with <i>Linux</i> and open source in general, and I'm changing distros a lot.
-                I would like to collaborate on open source projects.</p>
+				<img className='imgCool imgMain' src={imagecode} alt='A computer with code and a cup' width='300' height='200'/>
+				<h2>About me</h2>
 
-                <p>My favourite language right now is <i>Python</i>. Currently using <i>Visual Studio Codium</i>.
-                Feel free to contact me if you want to talk, collaborate or correct me on something.</p>
+				<p>
+					Welcome to my webpage, I'm just a guy who likes to develop software and webapps, play games 
+					and listen to music; I like to tinker with <i>Linux</i> and open source in general. I can speak 
+					Spanish and English. <br/><br/> My favourite language at the moment is <i>Python</i>. Feel free to contact 
+					me if you want to talk, collaborate or correct me on something.
+				</p>
 
-                <br/>
+				<br/>
 
-                <h3>I have knowledge in software development with technologies such as:</h3>
-                
-                <i>
-                <ul>
-                    <li className='oneLine'>Python</li>
-                    <li className='oneLine'>Java</li>
-                    <li className='oneLine'>React</li>
-                    <li className='oneLine'>SQL</li>
-                </ul>
-                </i>
+				<h3>I have knowledge in software development with technologies such as:</h3>
 
-            </div>
+				<i>
+					<ul>
+						<li className='oneLine'>React</li>
+						<li className='oneLine'>Python</li>
+						<li className='oneLine'>Java</li>
+						<li className='oneLine'>SQL</li>
+					</ul>
+				</i>
 
-            <div>
-                <div className='rSideMain mar'>
-                    <h2>Contact</h2>
+			</div>
 
-                    <ul>
-                        <li>
-                            Github:&nbsp;
-                            <a target='_blank' rel="noreferrer" href='https://github.com/Aleksei324' title='Link to my personal Github.'>Aleksei324</a>
-                        </li>
-                        <li>
-                            LinkedIn:&nbsp;
-                            <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/aleksei324' title='Link to my personal LinkedIn.'>Aleksei324</a>
-                        </li>
-                    </ul>
+			<div>
+				<div className='rSideMain mar'>
+					<h2>Contact</h2>
 
-                </div>
-                <div>{/* This div helps the side panel to keep his normal size even when the text is stretched */}</div>
-            </div>
-            
-        </section>
-    );
+					<ul>
+						<li>
+							Github:&nbsp;
+							<a target='_blank' rel="noreferrer" 
+								href='https://github.com/Aleksei324' 
+								title='Link to my personal Github.'>Aleksei324</a>
+						</li>
+
+						<li>
+							LinkedIn:&nbsp;
+							<a target='_blank' rel="noreferrer" 
+								href='https://www.linkedin.com/in/aleksei324' 
+								title='Link to my personal LinkedIn.'>Aleksei324</a>
+						</li>
+					</ul>
+
+				</div>
+
+				<div>
+					{/* This div helps the side panel to keep his normal size even when the text is stretched */}
+				</div>
+
+			</div>
+
+		</section>
+	);
 }
 
 export default AboutMe;
